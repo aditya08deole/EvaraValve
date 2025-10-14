35c632fb0051a4c8ccf4e2fc61e7448f1d30007b
Adi Deole <adityadeole08@gmail.com>
2025-10-14 16:09:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const BLYNK_API_BASE = 'https://blynk.cloud/external/api';
 
 // --- Polling and Offline Detection Configuration ---
 // MODIFIED: Polling rate is faster for a more responsive UI
-const POLLING_RATE_MS = 500;
+const POLLING_RATE_MS = 1000;
 const STALE_POLL_THRESHOLD = 20;
 
 // --- VIRTUAL PIN DEFINITIONS (v8.0 Update) ---
